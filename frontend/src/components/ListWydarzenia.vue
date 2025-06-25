316472aaf3a6ea7ee090d8ed5de825d4ef4871c4
maxa <maxabrams2004@gmail.com>
2025-06-25 14:11:42 +0200

    css poprawki

--- frontend/src/components/ListWydarzenia.vue
+++ frontend/src/components/ListWydarzenia.vue
@@ -4,6 +4,9 @@ import { reactive, onMounted, ref, watch, defineComponent} from 'vue'
 import dayjs from 'dayjs'
 import 'dayjs/locale/pl'
 import { computed } from 'vue';
+import { useDisplay } from 'vuetify/lib/composables/display';
+
+const display = useDisplay();
 
 dayjs.locale('pl')
 
@@ -26,17 +29,16 @@ function getCurrentYear() {
   return new Date().getFullYear()
 }
 
-function filteringByYear(date) {
-    if (chosedYears.value.length === 0) {
-        console.log(chosedYears.length)
-        return true;
-    }
-    return chosedYears.value.includes(+date.year)
-}
+
+
+const currentPage = ref(1)
+
 const dateList = ref([])
+const numberOfPages = ref(0)
 
 const chosedYears = ref([])
 async function fetchItems(){
+    numberOfPages.value = 0
     console.log('Żyję!')
     const params = new URLSearchParams();
     chosedYears.value.forEach(year => {
@@ -45,13 +47,20 @@ async function fetchItems(){
 
     const response = await fetch(`/api/all?${params.toString()}`);
     const data = await response.json();
-    dateList.value = data;
-    console.log(data);
-
+    dateList.value = await data;
+    numberOfPages.value = await Math.ceil((dateList.value.length) / 12)
 }
 
+const paginatedList = computed (()=>{
+    const s = (currentPage.value - 1) * 12;
+    const e = s + 12;
+    return dateList.value.slice(s, e);
+})
+
 watch(chosedYears, () => {
+    currentPage.value = 1
     fetchItems();
+    
 })
 
 onMounted(()=>{
@@ -77,19 +86,28 @@ onMounted(()=>{
             variant="outlined"
         ></v-select>
         <div class="eventList">
-            <div v-for="date in dateList" :key='date.id'>
+            <div v-for="date in paginatedList" :key='date.id'>
                 
                 <v-card 
                     class="eventCard"
-                    :title= 'dayjs(date.DataStart).format("HH:mm")'
-                    :subtitle="localDate(date.DataStart)"
-                    :text="date.Nazwa"
+                    
                     variant="flat">
+                    <v-card-title></v-card-title>
+                    <v-card-subtitle
+                    style="padding-top: 5px;"
+                    >   
+                        {{ localDate(date.DataStart) }}
+                    </v-card-subtitle>
+                    <v-card-text class="two-lines-ellipsis"
+                    style="font-size: 1rem; font-weight: bold;">
+                        {{ date.Nazwa }}
+                    </v-card-text>
                     <v-card-actions>
+
                         <router-link :to="`/kalendarium/${date.Alias}`">
-                        <v-btn>
+                        <v-btn style="text-transform: none;">
                             <v-icon icon="$next" start/>
-                            Zobacz więcej
+                            Więcej
                         </v-btn>
                         </router-link>
                     </v-card-actions>
@@ -105,14 +123,42 @@ onMounted(()=>{
             </div>
         </div>
     </main>
-    <v-pagination></v-pagination>
+    <div class="pagination">
+        <v-pagination 
+            v-if="numberOfPages > 1"
+            v-model = currentPage
+            :length = numberOfPages
+            :total-visible="display.smAndDown ? 3 : 5"  
+            rounded="circle"
+        >
+        </v-pagination>
+    </div>
 </template>
 
 <style>
+    .two-lines-ellipsis {
+        display: -webkit-box;
+        -webkit-line-clamp: 2;
+        -webkit-box-orient: vertical;
+        overflow: hidden;
+        text-overflow: ellipsis;
+        white-space: normal;
+        line-height: 2rem;
+        max-height: 3.9rem;  
+}
 
+
+    .v-card{
+        flex: 1 1;
+        height: 100%
+    }
     .eventList {
         display: flex;
         flex-flow: row wrap;
+        align-items: stretch;
+    }
+    .eventList>div{
+        margin: 5px;
     }
     li {
         text-decoration: none;
@@ -127,6 +173,11 @@ onMounted(()=>{
     }
     .module-event-date {
         margin-bottom: 10px;
+    }
+    @media (max-width:680px) {
+    .eventList{
+        display: block;    
+    }
     }
     
 </style>
